46dba1bd58016a1a9857a1900861b8bc09786713
Guilherme Webber <gwm131196@hotmail.com>
2016-07-07 15:05:28 -0300

    criando schemas

---
+++ transactions/transaction.schema.js
@@ -0,0 +1,29 @@
+/**
+ *User Schema 
+ * 
+ */
+var mongoose = require('mongoose');
+ 
+var userSchema = mongoose.Schema({
+	phone: String,
+	name: String,
+	registration_id: String,
+	registration_flag: Boolean	
+});
+
+userSchema.pre('save', function(next){
+//get current date
+var currentDate = new Date();
+
+//change the updated_at fielt to current date
+this.updated_at = currentDate;
+
+// if created_at doesn't exist
+if(!this.created_at)
+    this.created_at = currentDate;
+
+next();
+
+});
+
+module.exports = mongoose.model('Users', userSchema);
